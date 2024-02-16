bd679183d9d419a2b846103c670b0d76d618f60a
Akin Evans <akinevans@gmail.com>
2024-02-15 17:37:59 -0800

    Initalize MainContent and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
+import MainContent from "./components/MainContent/MainContent";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className='App'>
-      <p className='text-blue-700 text-navSize font-inter '>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nam
-        dolorem exercitationem, repellat quia iure. Voluptate accusamus quasi
-        voluptatem vitae!
-      </p>
+    <div className='app-wrapper max-w-[1000px] h-auto'>
+      <Navbar />
+      <MainContent />
     </div>
   );
 }
